19b4c848803bb639cc82b88d70d82f37feb679e1
AlexPeshkoff <alexander.peshkoff@gmail.com>
2026-05-07 18:19:37 +0300

    Fixed reported to client text type information for arrays

--- src/dsql/dsql.cpp
+++ src/dsql/dsql.cpp
@@ -1395,6 +1395,11 @@ dsql_rel::dsql_rel(MemoryPool& p, jrd_rel* jrel)
 			fld->elementDtype = array->arr_desc.iad_rpt[0].iad_desc.dsc_dtype;
 			fld->elementLength = array->arr_desc.iad_element_length;
 			fld->dimensions = array->arr_desc.iad_dimensions;
+
+			dsc tmp(array->arr_desc.iad_rpt[0].iad_desc);
+			fld->collationId = tmp.getCollation();
+			fld->textType = tmp.getTextType();
+			fld->charSetId = tmp.getCharSet();
 		}
 
 		auto** iter = &rel_fields;
